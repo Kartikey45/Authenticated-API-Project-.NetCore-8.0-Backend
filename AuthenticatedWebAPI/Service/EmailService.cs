d5e9bf541ba2f5ecb62b05f314ecfdf717e2072a
Kartikey45 <singhkartikey45@gmail.com>
2024-06-25 16:54:43 +0530

    --- code pushed ---

---
+++ AuthenticatedWebAPI/Service/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using AuthenticatedWebAPI.Models;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace AuthenticatedWebAPI.Service
+{
+    public class EmailService
+    {
+        private const string templatePath = @"EmailTemplate/{0}.html";
+        private readonly SMTPConfigModel _smtpConfigModel;
+
+        public EmailService(IOptions<SMTPConfigModel> smtpConfig)
+        {
+            _smtpConfigModel = smtpConfig.Value;
+        }
+
+        private async Task SendEmail(UserEmailOptions userEmailOptions)
+        {
+            MailMessage mail = new MailMessage()
+            {
+                Subject = userEmailOptions.Subject,
+                Body = userEmailOptions.Body,
+                From = new MailAddress(_smtpConfigModel.SenderAddress, _smtpConfigModel.SenderDisplayName),
+                IsBodyHtml = _smtpConfigModel.IsBodyHtml
+            };
+
+            foreach (var toEmail in userEmailOptions.ToEmails)
+            {
+                mail.To.Add(toEmail);
+            }
+            NetworkCredential networkCredential = new NetworkCredential(_smtpConfigModel.UserName, _smtpConfigModel.Password);
+            SmtpClient smtpClient = new SmtpClient()
+            {
+                Host = _smtpConfigModel.Host,
+                Port = _smtpConfigModel.Port,
+                EnableSsl = _smtpConfigModel.EnableSSL,
+                UseDefaultCredentials = _smtpConfigModel.UseDefaultCredentials,
+                Credentials = networkCredential
+            };
+            mail.BodyEncoding = Encoding.Default;
+            await smtpClient.SendMailAsync(mail);
+        }
+
+        private string GetEmailBody(string templateName) {
+            var body = File.ReadAllText(string.Format(templatePath, templateName));
+            return body;
+        }
+
+    }
+}
